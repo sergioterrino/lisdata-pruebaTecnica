7f8b7861c203f6dd2f967f6f98e7e0424e9d6ec1
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-09-27 21:36:36 +0200

    dev3 commit - learning somethings

--- src/main/java/com/lisdatasolutions/lisdata/user/UserRepository.java
+++ src/main/java/com/lisdatasolutions/lisdata/user/UserRepository.java
@@ -3,9 +3,16 @@ package com.lisdatasolutions.lisdata.user;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-  List<User> findByActiveTrue();
+  List<User> findByActiveTrue(); // query method
+  // the same but with JPQL
+  @Query("SELECT u from User u where u.active = true")
+  List<User> findByActiveTrueJPQL();
+  // the same but with the native
+  @Query(value="SELECT * from user where active = true", nativeQuery=true)
+  List<User> findByActiveTrueNative();
 }
--- src/main/java/com/lisdatasolutions/lisdata/user/UserService.java
+++ src/main/java/com/lisdatasolutions/lisdata/user/UserService.java
@@ -1,14 +1,15 @@
 package com.lisdatasolutions.lisdata.user;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lisdatasolutions.lisdata.dto.UserWithInactiveVehicles;
+import com.lisdatasolutions.lisdata.vehicle.Vehicle;
 import com.lisdatasolutions.lisdata.vehicle.VehicleRepository;
-
 
 @Service
 public class UserService {
@@ -72,27 +73,42 @@ public class UserService {
     }
   }
 
-  // Esto es usando el query method 
+  // Esto es usando el query method
   public List<UserWithInactiveVehicles> getActiveUsersWithInactiveVehicles() {
     List<User> activeUsers = userRepository.findByActiveTrue();
     return activeUsers.stream()
-            .map(user -> new UserWithInactiveVehicles(user, vehicleRepository.findByUserIdAndActiveFalse(user.getId())))
-            .collect(Collectors.toList());
+        .map(user -> new UserWithInactiveVehicles(user, vehicleRepository.findByUserIdAndActiveFalse(user.getId())))
+        .collect(Collectors.toList());
   }
-  
+
   // Esto es usando la lista de vehiculos, no es tan eficiente
-  // public List<UserWithInactiveVehicles> getActiveUsersWithInactiveVehicles1() {
-  //   List<User> activeUsers = userRepository.findByActiveTrue();
-  //   List<UserWithInactiveVehicles> usersWithInactiveVehicles1 = new ArrayList<>();
-  //   for (User user : activeUsers) {
-  //     // para cada user activo, el getVehicles() trae todos los vehiculos
-  //     // el stream() convierte la lista de vehiculos en un stream para poder filtrarlos
-  //     // el filter() filtra los vehiculos inactivos
-  //     // el collect() convierte el stream en una lista.
-  //     List<Vehicle> inactiveVehicles = user.getVehicles().stream().filter(vehicle -> !vehicle.isActive()).collect(Collectors.toList());
-  //     usersWithInactiveVehicles1.add(new UserWithInactiveVehicles(user, inactiveVehicles));
-  //   }
-  //   return usersWithInactiveVehicles1;
-  // }
+  public List<UserWithInactiveVehicles> getActiveUsersWithInactiveVehicles1() {
+    List<User> activeUsers = userRepository.findByActiveTrue();
+    List<UserWithInactiveVehicles> usersWithInactiveVehicles1 = new ArrayList<>();
+    for (User user : activeUsers) {
+      // para cada user activo, el getVehicles() trae todos los vehiculos
+      // el stream() convierte la lista de vehiculos en un stream para poder
+      // filtrarlos
+      // el filter() filtra los vehiculos inactivos
+      // el collect() convierte el stream en una lista.
+      List<Vehicle> inactiveVehicles = user.getVehicles().stream()
+          .filter(vehicle -> !vehicle.isActive())
+          .collect(Collectors.toList());
+      usersWithInactiveVehicles1.add(new UserWithInactiveVehicles(user, inactiveVehicles));
+    }
+    return usersWithInactiveVehicles1;
+  }
+
+  // it's the same but with streams
+  public List<UserWithInactiveVehicles> getActiveUsersWithInactiveVehicles2() {
+    List<User> users = userRepository.findByActiveTrue();
+    return users.stream()
+        .map(user -> {
+          List<Vehicle> vehicles = user.getVehicles().stream()
+              .filter(v -> !v.isActive())
+              .collect(Collectors.toList());
+          return new UserWithInactiveVehicles(user, vehicles);
+        }).collect(Collectors.toList());
+  }
 
 }
